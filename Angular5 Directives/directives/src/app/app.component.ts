08cbdfbe6f3de9048b8318e5cdd05cc4da619231
Kaloian Ianev <kianev@gmail.com>
2018-03-16 08:43:00 +0100

    *ngFor directive and trackBy for performance improvement

--- Angular5 Directives/directives/src/app/app.component.ts
+++ Angular5 Directives/directives/src/app/app.component.ts
@@ -6,19 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'},
-  ];
+  courses;
   viewMode = 'something';
 
-  onAdd() {
+/*  onAdd() {
     this.courses.push({id: 4, name: 'course4'})
   }
 
   onRemove(course) {
    let index = this.courses.indexOf(course);
    this.courses.splice(index, 1);
+  }*/
+
+  loadCourses() {
+    this.courses = [
+      {id: 1, name: 'course1'},
+      {id: 2, name: 'course2'},
+      {id: 3, name: 'course3'},
+    ];
+  }
+
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
   }
 }
