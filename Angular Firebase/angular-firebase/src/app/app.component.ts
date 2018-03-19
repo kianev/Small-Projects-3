846d4731290574a9912e3ef185047668378ddb46
Kaloian Ianev <kianev@gmail.com>
2018-03-19 11:25:46 +0100

    New Angular 5 project conntected with Firebase using AngularFire2, successfully reading data from database

---
+++ Angular Firebase/angular-firebase/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {AngularFireDatabase} from "angularfire2/database";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+   courses: any[];
+
+   constructor(db: AngularFireDatabase) {
+     db.list('courses').valueChanges()
+       .subscribe(courses => {
+         this.courses = courses;
+         console.log(courses);
+       })
+   }
+}
---
+++ Angular Firebase/angular-firebase/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDJKVynmh-1UDTuRf4y0A0NavEonwMGbnQ",
+    authDomain: "fir-test-76fda.firebaseapp.com",
+    databaseURL: "https://fir-test-76fda.firebaseio.com",
+    projectId: "fir-test-76fda",
+    storageBucket: "fir-test-76fda.appspot.com",
+    messagingSenderId: "481864013110"
+  }
+};
