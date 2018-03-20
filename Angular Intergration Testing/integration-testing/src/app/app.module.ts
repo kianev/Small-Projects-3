614a91a65fa3eae933b14530d995f9eab5cb4f41
Kaloian Ianev <kianev@gmail.com>
2018-03-20 19:12:38 +0100

    Integration testing in Angular, testing voter component

---
+++ Angular Intergration Testing/integration-testing/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { TodosComponent } from './2-todos/todos.component';
+import { UserDetailsComponent } from './3-user-details/user-details.component';
+import { VoterComponent } from './1-voter/voter.component';
+
+import { routes } from './app.routes';
+import { UsersComponent } from './users/users.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+//import { NavComponent } from './nav/nav.component';
+import { HighlightDirective } from './highlight.directive';
+import { GreeterComponent } from './greeter/greeter.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TodosComponent,
+    UserDetailsComponent,
+    VoterComponent,
+    UsersComponent,
+    GreeterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
