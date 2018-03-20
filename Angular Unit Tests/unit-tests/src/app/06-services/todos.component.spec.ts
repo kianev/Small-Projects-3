b48feff576ff2544a8a055141f8e35445a5e38ab
Kaloian Ianev <kianev@gmail.com>
2018-03-20 17:14:39 +0100

    unit testing of todo component, all methods

--- Angular Unit Tests/unit-tests/src/app/06-services/todos.component.spec.ts
+++ Angular Unit Tests/unit-tests/src/app/06-services/todos.component.spec.ts
@@ -2,6 +2,8 @@ import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/throw';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -21,5 +23,42 @@ describe('TodosComponent', () => {
     component.ngOnInit();
 
     expect(component.todos).toBe(todos);
+  });
+
+  it('should call the server when new item is added to save the changes', () => {
+    let spy = spyOn(service, 'add').and.callFake(t => {
+      return Observable.empty();
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should set message if server returns an error', () => {
+    let error = 'error from the server';
+    let spy = spyOn(service, 'add').and.returnValue(Observable.throw(error));
+
+    component.add();
+
+    expect(component.message).toBe(error);
+  });
+
+  it('should call the server to delete a todo item if the user confirms', () => {
+    spyOn(window, 'confirm').and.returnValue(true);
+    let spy = spyOn(service, 'delete').and.returnValue(Observable.empty());
+
+    component.delete(1);
+
+    expect(spy).toHaveBeenCalledWith(1);
+  });
+
+  it('should not call the server to delete a todo item if the user cancels', () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+    let spy = spyOn(service, 'delete').and.returnValue(Observable.empty());
+
+    component.delete(1);
+
+    expect(spy).not.toHaveBeenCalled();
   });
 });
